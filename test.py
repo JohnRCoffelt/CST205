ad45637fef108a5609e9c7da8bea7532a4b3c225
CodeBloodedChris <33336510+CodeBloodedChris@users.noreply.github.com>
2017-11-02 19:20:52 -0700

    First Test
    Testing my first file in my repo.

---
+++ test.py
@@ -0,0 +1,16 @@
+def get_pic():
+  return makePicture(pickAFile())
+  
+# function to create a better black and white image  
+def betterBnW():
+  pic = get_pic()
+  pixels = getPixels(pic)
+  for p in pixels:
+    lumin = getRed(p)*0.299 + getBlue(p)*0.114 + getGreen(p)*0.587
+    setColor(p, makeColor(lumin, lumin, lumin))
+  repaint(pic)
+
+
+
+
+
